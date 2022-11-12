b0170b9f4d89adc9a0697ffb7fb6aedd849f1e48
Konstantin Polevik <k.polevik@gmail.com>
2022-11-12 15:21:21 +0300

    Support for F-keys added

--- include/rtl/sys/impl/keyboard.hpp
+++ include/rtl/sys/impl/keyboard.hpp
@@ -4,9 +4,9 @@
  *
  * This file is part of the RTL library. Redistribution and use in source and
  * binary forms, with or without modification, are permitted exclusively
- * under the terms of the MIT license. You should have received a copy of the 
+ * under the terms of the MIT license. You should have received a copy of the
  * license with this file. If not, please visit:
- * https://github.com/out61h/rtl/blob/main/LICENSE. 
+ * https://github.com/out61h/rtl/blob/main/LICENSE.
  */
 #pragma once
 
@@ -32,6 +32,42 @@ namespace rtl
 
                 case VK_SPACE:
                     return keyboard::keys::space;
+
+                case VK_F1:
+                    return keyboard::keys::f1;
+
+                case VK_F2:
+                    return keyboard::keys::f2;
+
+                case VK_F3:
+                    return keyboard::keys::f3;
+
+                case VK_F4:
+                    return keyboard::keys::f4;
+
+                case VK_F5:
+                    return keyboard::keys::f5;
+
+                case VK_F6:
+                    return keyboard::keys::f6;
+
+                case VK_F7:
+                    return keyboard::keys::f7;
+
+                case VK_F8:
+                    return keyboard::keys::f8;
+
+                case VK_F9:
+                    return keyboard::keys::f9;
+
+                case VK_F10:
+                    return keyboard::keys::f10;
+
+                case VK_F11:
+                    return keyboard::keys::f11;
+
+                case VK_F12:
+                    return keyboard::keys::f12;
                 }
 
                 return keyboard::keys::any;
--- include/rtl/sys/keyboard.hpp
+++ include/rtl/sys/keyboard.hpp
@@ -21,6 +21,18 @@ namespace rtl
             enter,
             esc,
             space,
+            f1,
+            f2,
+            f3,
+            f4,
+            f5,
+            f6,
+            f7,
+            f8,
+            f9,
+            f10,
+            f11,
+            f12,
             count
         };
     } // namespace keyboard
