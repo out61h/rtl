ad94b9cbd7bb13dc4b3e8ea26da6f0255c499398
Konstantin Polevik <k.polevik@gmail.com>
2022-11-15 00:57:23 +0300

    Add unique_ptr<T[]> specialization

--- include/rtl/memory.hpp
+++ include/rtl/memory.hpp
@@ -69,6 +69,23 @@ namespace rtl
             static constexpr bool value = true;
         };
 
+        template<typename>
+        struct is_array
+        {
+            static constexpr bool value = false;
+        };
+
+        template<typename T, size_t Size>
+        struct is_array<T[Size]>
+        {
+            static constexpr bool value = true;
+        };
+
+        template<typename T>
+        struct is_array<T[]>
+        {
+            static constexpr bool value = true;
+        };
     } // namespace impl
 
     template<typename T, typename D = impl::default_deleter<T>>
@@ -184,6 +201,118 @@ namespace rtl
         deleter_type m_deleter;
     };
 
-    // TODO: specialization for [] with operator[]
+    template<typename T, typename D>
+    class unique_ptr<T[], D> final
+    {
+    public:
+        using element_type = T;
+        using deleter_type = D;
+        using pointer = typename impl::pointer<T>::type;
+
+        constexpr unique_ptr()
+            : m_ptr( pointer() )
+            , m_deleter( deleter_type() )
+        {
+            static_assert( !impl::is_pointer<deleter_type>::value,
+                           "constructed with null function pointer deleter" );
+        }
+
+        constexpr explicit unique_ptr( pointer p )
+            : m_ptr( p )
+            , m_deleter( deleter_type() )
+        {
+            static_assert( !impl::is_pointer<deleter_type>::value,
+                           "constructed with null function pointer deleter" );
+        }
+
+        constexpr explicit unique_ptr( pointer p, deleter_type deleter )
+            : m_ptr( p )
+            , m_deleter( deleter )
+        {
+        }
+
+        constexpr explicit unique_ptr( pointer p, nullptr_t ) = delete;
+
+        ~unique_ptr()
+        {
+            reset();
+        }
+
+        constexpr unique_ptr( unique_ptr&& other )
+            : m_ptr( other.release() )
+            , m_deleter( other.m_deleter )
+        {
+        }
+
+        constexpr unique_ptr& operator=( unique_ptr&& other )
+        {
+            if ( this != &other )
+            {
+                reset( other.release() );
+                m_deleter = other.m_deleter;
+            }
+
+            return *this;
+        }
+
+        [[nodiscard]] constexpr const element_type& operator[]( size_t index ) const
+        {
+            return *( m_ptr + index );
+        }
+
+        [[nodiscard]] constexpr element_type& operator[]( size_t index )
+        {
+            return *( m_ptr + index );
+        }
+
+        [[nodiscard]] constexpr explicit operator bool() const
+        {
+            return m_ptr != nullptr;
+        }
+
+        [[nodiscard]] constexpr const pointer get() const
+        {
+            return m_ptr;
+        }
+
+        [[nodiscard]] constexpr pointer get()
+        {
+            return m_ptr;
+        }
+
+        constexpr void reset( pointer p = pointer() )
+        {
+            pointer old_ptr = m_ptr;
+            m_ptr = p;
+
+            if ( old_ptr )
+                m_deleter( old_ptr );
+        }
+
+        [[nodiscard]] constexpr pointer release()
+        {
+            pointer result = m_ptr;
+            m_ptr = pointer();
+            return result;
+        }
+
+        [[nodiscard]] constexpr bool operator==( nullptr_t ) const
+        {
+            return m_ptr == nullptr;
+        }
+
+        [[nodiscard]] constexpr bool operator!=( nullptr_t ) const
+        {
+            return m_ptr != nullptr;
+        }
+
+    private:
+        unique_ptr( const unique_ptr& ) = delete;
+        unique_ptr& operator=( const unique_ptr& ) = delete;
+
+        pointer      m_ptr;
+        deleter_type m_deleter;
+    };
+
     // TODO: make_unique
 } // namespace rtl
