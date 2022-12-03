670838ff6a4d6a1162de13dc6a50e3d84e1200a7
Konstantin Polevik <k.polevik@gmail.com>
2022-12-04 02:27:02 +0300

    Add file header

--- include/rtl/vector.hpp
+++ include/rtl/vector.hpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2016-2022 Konstantin Polevik
+ * All rights reserved
+ *
+ * This file is part of the RTL library. Redistribution and use in source and
+ * binary forms, with or without modification, are permitted exclusively
+ * under the terms of the MIT license. You should have received a copy of the
+ * license with this file. If not, please visit:
+ * https://github.com/out61h/rtl/blob/main/LICENSE.
+ */
 #pragma once
 
 #include <rtl/algorithm.hpp>
